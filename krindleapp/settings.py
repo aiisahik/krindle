a72b2ded9751d58e046f4c639b1b270454ce8f22
Johnny Jiang <racketjo@Johnnys-MacBook-Air.local>
2014-03-30 17:11:29 -0400

    modified settings.py

--- krindleapp/settings.py
+++ krindleapp/settings.py
@@ -54,12 +54,12 @@ WSGI_APPLICATION = 'krindleapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
