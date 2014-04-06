2dcddbaf001596265382ca5080f98a007d4fb2ac
aiisahik <jie.z.zhou@gmail.com>
2014-04-06 18:18:10 -0400

    admin view for UserProfile

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import UserProfile
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(UserProfile, UserProfileAdmin)
