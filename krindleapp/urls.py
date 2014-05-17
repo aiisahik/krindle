e64806f7a2e8c5d3ec4e3a97e88e51b0fa63c11b
aiisahik <jie.z.zhou@gmail.com>
2014-05-17 16:58:42 -0400

    added app django view

--- krindleapp/urls.py
+++ krindleapp/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'krindleapp.views.home', name='home'),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^app/', TemplateView.as_view(template_name="app.html")),
     url(r'^account/', include('account.urls')),
     url(r'^match/', TemplateView.as_view(template_name="main.html")),
-
     url(r'^admin/', include(admin.site.urls)),
 )
